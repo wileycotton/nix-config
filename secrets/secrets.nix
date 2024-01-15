b94c0d50f6c325b5191125e9749c589f01874924
Bob Cotton <bob.cotton@gmail.com>
2024-01-15 09:26:40 -0700

    rekeyed for nix-02

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,9 +3,12 @@ let
   users = [ bcotton ];
 
   admin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjeiDeFxI7BcbjDxtPyeWfsUWBW2HKTyjT8/X0719+p root@nixos";
-  systems = [ admin ];
+  nix-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQZ30jhjs15gaZkJcKsKXvNqkvgF/rwmKLqcj7rSvCj root@nix-01";
+  nix-02 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMVfXXmolLt0FWt5Qw9ihT0XObNM/YKkUZEnjYFEs1Bu root@nix-02";
+
+  systems = [ admin nix-01 nix-02 ];
 in
 {
-  "librespot.age".publicKeys = [ admin bcotton ];
-  "mopidy.age".publicKeys = [ admin bcotton ];
+  "librespot.age".publicKeys = users ++ systems;
+  "mopidy.age".publicKeys = users ++ systems;
 }
