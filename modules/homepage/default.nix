17b8403f17aab16b471f7b132bf893e6589b3e38
Bob Cotton <bob.cotton@gmail.com>
2024-05-21 16:43:23 -0600

    various dns changes

--- modules/homepage/default.nix
+++ modules/homepage/default.nix
@@ -4,7 +4,34 @@
   ...
 }: let
 in {
-  services.homepage-dashboard.enable = true;
+  services.homepage-dashboard = {
+    enable = true;
+    openFirewall = true;
+    # bookmarks = [
+    #   {
+    #     dev = [
+    #       {
+    #         github = [
+    #           {
+    #             abbr = "GH";
+    #             href = "https://github.com/";
+    #             icon = "github-light.png";
+    #           }
+    #         ];
+    #       }
+    #       {
+    #         "homepage docs" = [
+    #           {
+    #             abbr = "HD";
+    #             href = "https://gethomepage.dev";
+    #             icon = "homepage.png";
+    #           }
+    #         ];
+    #       }
+    #     ];
+    #   }
+    # ];
+  };
 
   # see also https://github.com/VTimofeenko/monorepo-machine-config/blob/1441574e424ca64f9abb33bc879aa4cd0e29145b/nixosModules/services/home-dashboard/homepage-dashboard.nix
 }
