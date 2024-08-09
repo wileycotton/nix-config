29cdcfa51108a75b90a83ed93558a237131f52c1
Bob Cotton <bob.cotton@gmail.com>
2024-08-09 08:20:08 -0600

    Added node-exporter to dns-01

--- hosts/nixos/dns-01/default.nix
+++ hosts/nixos/dns-01/default.nix
@@ -10,6 +10,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../../modules/node-exporter
     ../../../modules/dnsmasq/dnsmasq.nix
   ];
 
