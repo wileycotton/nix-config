75a029ec484cd09589f308aed6462e8d25dfdf1d
wiley-cotton <thomaswileycotton@gmail.com>
2024-11-21 18:58:49 -0700

    reordering defaults vs by user

--- hosts/common/darwin-common.nix
+++ hosts/common/darwin-common.nix
@@ -6,5 +6,34 @@
   ...
 }: {
   config = {
+        # These are packages are just for darwin systems
+    environment.systemPackages = [
+      pkgs.kind
+      unstablePkgs.esphome
+    ];
+
+    system.stateVersion = 5;
+
+    nix = {
+      #package = lib.mkDefault pkgs.unstable.nix;
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+        warn-dirty = false;
+      };
+    };
+    services.nix-daemon.enable = true;
+
+    # pins to stable as unstable updates very often
+    nix.registry.nixpkgs.flake = inputs.nixpkgs;
+    nix.registry = {
+      n.to = {
+        type = "path";
+        path = inputs.nixpkgs;
+      };
+      u.to = {
+        type = "path";
+        path = inputs.nixpkgs-unstable;
+      };
+    };
   };
 }
--- hosts/darwin/bobs-laptop/default.nix
+++ hosts/darwin/bobs-laptop/default.nix
@@ -9,36 +9,7 @@
   inherit (inputs) nixpkgs nixpkgs-unstable;
 in {
   config = {
-    # These are packages are just for darwin systems
-    environment.systemPackages = [
-      pkgs.kind
-      unstablePkgs.esphome
-    ];
-
     users.users.bcotton.home = "/Users/bcotton";
-    system.stateVersion = 5;
-
-    nix = {
-      #package = lib.mkDefault pkgs.unstable.nix;
-      settings = {
-        experimental-features = ["nix-command" "flakes"];
-        warn-dirty = false;
-      };
-    };
-    services.nix-daemon.enable = true;
-
-    # pins to stable as unstable updates very often
-    nix.registry.nixpkgs.flake = inputs.nixpkgs;
-    nix.registry = {
-      n.to = {
-        type = "path";
-        path = inputs.nixpkgs;
-      };
-      u.to = {
-        type = "path";
-        path = inputs.nixpkgs-unstable;
-      };
-    };
 
     nixpkgs.config.allowUnfree = true;
     nixpkgs.config.overlays = [
