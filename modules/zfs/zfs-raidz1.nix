7ae97e42654232045624c0199574165f3067cc1e
Bob Cotton <bob.cotton@gmail.com>
2024-12-28 14:57:37 -0700

    Copied options to both configs

--- modules/zfs/zfs-raidz1.nix
+++ modules/zfs/zfs-raidz1.nix
@@ -51,14 +51,34 @@ in {
           type = "zpool";
           mode = "raidz1";
           rootFsOptions = {
-            acltype = "posixacl";
+            # https://jrs-s.net/2018/08/17/zfs-tuning-cheat-sheet/
+            atime = "off"; # check
+            acltype = "posixacl"; 
+            compression = "lz4"; # check
+            xattr = "sa"; # check
+            recordsize = "64k"; # check
+            dnodesize = "auto";
+            canmount = "off";
+            relatime = "on";
+            normalization = "formD";
+            mountpoint = "none";
+            "com.sun:auto-snapshot" = "false";
           };
+          options = {
+            # TODO How to know if this should be 12 or 13?
+            # ashift=12 means 4K sectors (used by most modern hard drives), 
+            # and ashift=13 means 8K sectors (used by some modern SSDs).
+            ashift = "12";
+            autotrim = "on";
+          };
+
           datasets = {
             database = {
               type = "zfs_fs";
               mountpoint = "/db";
               options = {
                 mountpoint = "legacy";
+                recordsize = "8k"; # for postgres
                 "com.sun:auto-snapshot" = "true";
               };
             };
