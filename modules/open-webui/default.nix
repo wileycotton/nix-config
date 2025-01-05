3fe26bf7d63d5375b5e8095818b9a49345a6be12
Bob Cotton <bob.cotton@gmail.com>
2025-01-05 06:24:49 -0700

    various small fixes

--- modules/open-webui/default.nix
+++ modules/open-webui/default.nix
@@ -4,68 +4,32 @@
   lib,
   config,
   ...
-}:
-with lib; let
-  cfg = config.services.clubcotton.open-webui;
-in {
-  options.services.clubcotton.open-webui = {
-    enable = mkEnableOption "Open WebUI database support";
-
-    package = mkOption {
-      type = types.package;
-      default = unstablePkgs.open-webui;
-      description = "Open WebUI package to use.";
+}: {
+  services.open-webui = {
+    enable = true;
+    package = unstablePkgs.open-webui;
+    host = "0.0.0.0";
+    # stateDir = "/mnt/docker_volumes/open-webui";
+    environment = {
+      WEBUI_AUTH = "True";
+      ENABLE_OLLAMA_API = "True";
+      OLLAMA_BASE_URL = "http://toms-mini:11434";
+      OLLAMA_API_BASE_URL = "http://toms-mini:11434";
     };
+    environmentFile = config.age.secrets.open-webui.path;
+  };
 
-    enableTailscale = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Whether to enable Tailscale support.";
-    };
+  services.tsnsrv = {
+    enable = true;
+    defaults.authKeyPath = config.age.secrets.tailscale-keys.path;
 
-    environment = mkOption {
-      type = types.attrsOf types.str;
-      default = {};
-      description = "Environment variables to set.";
-    };
-
-    environmentFile = mkOption {
-      type = types.nullOr types.path;
-      default = null;
-      description = "Path to the environment file.";
-    };
-
-    stateDir = mkOption {
-      type = types.path;
-      default = "./data";
-      description = "State directory for Open WebUI.";
+    services.llm = {
+      ephemeral = true;
+      toURL = "http://${config.services.open-webui.host}:${toString config.services.open-webui.port}/";
     };
   };
 
-  config = mkIf cfg.enable {
-    services.open-webui = {
-      enable = true;
-      package = cfg.package;
-      host = "0.0.0.0";
-      stateDir = cfg.stateDir;
-      environment = {
-        WEBUI_AUTH = "True";
-      };
-      environmentFile = config.age.secrets.open-webui.path;
-    };
-
-    services.tsnsrv = mkIf cfg.enableTailscale {
-      enable = true;
-      defaults.authKeyPath = config.age.secrets.tailscale-keys.path;
-
-      services.llm = {
-        ephemeral = true;
-        toURL = "http://${config.services.open-webui.host}:${toString config.services.open-webui.port}/";
-      };
-    };
-
-    age.secrets."open-webui" = {
-      file = ../../secrets/open-webui.age;
-    };
+  age.secrets."open-webui" = {
+    file = ../../secrets/open-webui.age;
   };
 }
--- users/tomcotton.nix
+++ users/tomcotton.nix
@@ -8,7 +8,7 @@
     shell = pkgs.zsh;
     isNormalUser = true;
     extraGroups = ["wheel" "docker"]; # Enable ‘sudo’ for the user.
-    hashedPassword = "$6$icZo8IyqPlu1YOgc$aRlFcb7dxOOmOebE/hYdLXWPEboyEm5sfBBJZopuRfD1Hu7MQYw0eQokQecb0n5HUgaGXRWMrs2TUqcZMIzC71";
+    hashedPassword = "$6$wpaUixsrLhBdbijm$Wi4KRo2smEcnDOb8vXpxSZJWPUBZyDdWQEtYkjDtqMBr25nFfNpk3IjBr816x/FdAxU7YlinKs5lKmJi3huNp0";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKW08oClThlF1YJ+ey3y8XKm9yX/45EtaM/W7hx5Yvzb tomcotton@Toms-MacBook-Pro.local"
     ];
