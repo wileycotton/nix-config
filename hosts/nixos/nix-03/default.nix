bb1ce8198a56935ade4312aece98254940bbcc6b
Bob Cotton <bob.cotton@gmail.com>
2025-01-06 19:32:18 -0700

    wip

--- hosts/nixos/nix-03/default.nix
+++ hosts/nixos/nix-03/default.nix
@@ -24,7 +24,7 @@
     enable = true;
     dockerCompat = true;
     dockerSocket.enable = true;
-    # Required for containers under podman-compose to be able to talk to each other.
+     # Required for containers under podman-compose to be able to talk to each other.
     defaultNetwork.settings.dns_enabled = true;
   };
 
