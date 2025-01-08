9fea7d51381ec554484dcf801817a62e9c8df09f
Bob Cotton <bob.cotton@gmail.com>
2025-01-08 13:16:21 -0700

    wip

--- hosts/nixos/nix-03/default.nix
+++ hosts/nixos/nix-03/default.nix
@@ -18,6 +18,15 @@
   ];
 
   services.clubcotton.services.tailscale.enable = true;
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+    dockerSocket.enable = true;
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
   services.k3s.role = lib.mkForce "agent";
 
   clubcotton.zfs_single_root = {
