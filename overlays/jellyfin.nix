b32efc14bdd0250812b1dcfb3821ef5f2d0246e3
Bob Cotton <bob.cotton@gmail.com>
2025-01-08 15:33:38 -0700

    Added /media/tomcotton and friends

--- overlays/jellyfin.nix
+++ overlays/jellyfin.nix
@@ -5,7 +5,6 @@
   unstablePkgs,
   ...
 }: final: prev: {
-
   # https://discourse.nixos.org/t/trouble-getting-quicksync-to-work-with-jellyfin/42275/2
   jellyfin-ffmpeg = prev.jellyfin-ffmpeg.override {
     ffmpeg_7-full = prev.ffmpeg_7-full.override {
