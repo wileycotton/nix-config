0ab412d883f31cf0550eda3fcd2f280a9cd64901
wiley-cotton <thomaswileycotton@gmail.com>
2025-01-10 12:06:58 -0700

    claude test script
    # Conflicts:
    #	flake.nix

---
+++ modules/webdav/test.nix
@@ -0,0 +1,46 @@
+{nixpkgs ? import <nixpkgs> {}}:
+nixpkgs.lib.makeTest {
+  name = "webdav";
+
+  nodes.machine = {config, pkgs, ...}: {
+    imports = [./default.nix];
+
+    services.clubcotton.webdav = {
+      enable = true;
+      user = "testuser";
+      password = "testpass";
+      scope = "/var/lib/webdav";
+    };
+
+    # Create test directory and file
+    systemd.tmpfiles.rules = [
+      "d /var/lib/webdav 0755 testuser webdav"
+      "f /var/lib/webdav/test.txt 0644 testuser webdav - hello"
+    ];
+  };
+
+  testScript = ''
+    start_all()
+
+    # Wait for webdav service to start
+    machine.wait_for_unit("webdav.service")
+    machine.wait_for_open_port(8080)
+
+    # Test authentication and file access
+    machine.succeed(
+      "curl -f -u testuser:testpass http://localhost:8080/test.txt | grep hello"
+    )
+
+    # Test authentication failure
+    machine.fail(
+      "curl -f -u wronguser:wrongpass http://localhost:8080/test.txt"
+    )
+
+    # Test file upload
+    machine.succeed(
+      "echo 'new content' > upload.txt",
+      "curl -f -u testuser:testpass -T upload.txt http://localhost:8080/upload.txt",
+      "curl -f -u testuser:testpass http://localhost:8080/upload.txt | grep 'new content'"
+    )
+  '';
+}
