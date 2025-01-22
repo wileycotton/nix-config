094512dd1f477ec9de528577568b3901cb759e13
wiley-cotton <153463207+wileycotton@users.noreply.github.com>
2025-01-21 17:04:41 -0700

    Nix 04 (#60)
    * init
    
    * fmt and root on nix-04
    
    * my ssh key
    
    * flake add
    
    * double defined openssh key
    
    * zfs config
    
    * simpler networking
    
    * just command for nixinate: just nixinate
    
    * fmt
    
    * code review

---
+++ hosts/nixos/nix-04/default.nix
@@ -0,0 +1,92 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+#
+# Nuke and pave this machine
+# nix run github:nix-community/nixos-anywhere -- --flake '.#nix-04' root@<host ip>
+{
+  config,
+  pkgs,
+  lib,
+  unstablePkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKW08oClThlF1YJ+ey3y8XKm9yX/45EtaM/W7hx5Yvzb tomcotton@Toms-MacBook-Pro.local"
+    ];
+  };
+
+  clubcotton.zfs_single_root = {
+    enable = true;
+    poolname = "rpool";
+    swapSize = "4G"; # 1/4 of 16G
+    disk = "/dev/disk/by-id/ata-X12_SSD_256GB_KT2023000020001117";
+    useStandardRootFilesystems = true;
+    reservedSize = "50GiB"; #0.20 of 256G
+  };
+
+  networking = {
+    hostId = "3fa4e0cb";
+    hostName = "nix-04";
+    defaultGateway = "192.168.5.1";
+    nameservers = ["192.168.5.220"];
+    interfaces.eno1.ipv4.addresses = [
+      {
+        address = "192.168.5.54";
+        prefixLength = 24;
+      }
+    ];
+  };
+
+  virtualisation.libvirtd.enable = true;
+
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  programs.zsh.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
